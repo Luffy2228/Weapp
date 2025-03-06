19f3c387b4f54c3c6c020cfaa51d3a422feb0fb6
Karthik <akashkarthik2228@gmail.com>
2025-03-07 00:25:41 +0530

    Initial commit

---
+++ webapp.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import getit
+
+todos = getit.get()
+
+def add_todo():
+    todo = st.session_state["new_todo"]+"\n"
+    todos.append(todo)
+    getit.write_todos(todos)
+
+st.title("my application")
+st.subheader("this app ")
+st.write("this app for the builder")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder = "something",
+                on_change=add_todo, key='new_todo')
+
